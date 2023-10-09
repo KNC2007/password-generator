9566b8fa086687b653d1d1f07a3b150a648deb50
KNC2007 <cnkalbach@gmail.com>
2023-10-08 22:00:33 -0400

    fixed undefined error and changed vars to strings

--- Develop/script.js
+++ Develop/script.js
@@ -1,12 +1,17 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
-var specialCharacters = ["!", "$", "%", "&", "'", "(", ")", "#", "*", "+", ",", "-", ".", "/", ":", "]", "[", ";", "<", "=", ">", "?", "@", "/", "^", "_", "`", "{", "|", "}", "~"];
+var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numbers = "1234567890";
+var specialCharacters = "!$%&'()#*+,-./:][;<=>?@/^_`{|}~"
 
-var characters = lowerCase[0] + upperCase[0] + numbers[0] + specialCharacters[0];
+// var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+// var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+// var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+// var specialCharacters = ["!", "$", "%", "&", "'", "(", ")", "#", "*", "+", ",", "-", ".", "/", ":", "]", "[", ";", "<", "=", ">", "?", "@", "/", "^", "_", "`", "{", "|", "}", "~"];
+
+// var userPassword = lowerCase[0] + upperCase[0] + numbers[0] + specialCharacters[0];
 
 
 
@@ -16,10 +21,6 @@ function writePassword() {
   // var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
-
- 
-
   var userPassword = " ";
 
   // Ask user for their choice in password length
@@ -27,23 +28,18 @@ function writePassword() {
   // change to number and not string
   userPasswordLength = parseInt(userPasswordLength)
   // if click cancel put here // then a CONFIRM pop up screen
-  // what is user enters number less than 8 and more than 128?!?!?! or leaves it blank or presses cancel**
+  // what is user leaves it blank or presses cancel**
   console.log(userPasswordLength)
 
-  if(userPasswordLength < 8) {
+  if(userPasswordLength < 8 || userPasswordLength > 128) {
     window.alert ("Insert a number between 8 and 128.")
+    return
   }
-  return
-
-  if(userPasswordLength > 128) {
-    window.alert("Insert a number between 8 and 128.")
-  }
-  return
   
   // need 4 ifs with 4 confirm Expression Conditionals
   var userLowerCase = window.confirm("Do you want your password to include lowercase letters?")
-  // if user pressed ok or true
-  // if user pressed cancel or false
+  // if user pressed ok or true or yes
+  // if user pressed cancel or false or no
   if(userLowerCase === true) {
     userPassword += lowerCase;
   }
@@ -71,19 +67,17 @@ function writePassword() {
   
   var password = " ";
   
-  for (var i = 0; i <= userPasswordLength; i++) {
-    password += userPassword = Math.floor(Math.random() * userPasswordLength.length)
+  // processing... (randomly generate characters)
+  for (var i = 0; i < userPasswordLength; i++) {
+    password += userPassword[Math.floor(Math.random() * userPassword.length)]
   }
-  return password
+  // return look up math random code (whatever goes in return will come out the other side on the generator)
+   passwordText.value = password;
+   return password
 
 }
 
-// processing... (randomly generate characters)
-// return password
-//   return "turtle"; // if turtle is a password
 
-
-  // return look up math random code (whatever goes in return will come out the other side on the generator)
 
 
 // Write password to the #password input
